628e5e752f8a6f660db3e179b4311f77e17dcaaf
MrFatra <ramadhanfatra2004@gmail.com>
2025-05-15 22:47:21 +0700

    fix login api

--- lib/services/auth/login.dart
+++ lib/services/auth/login.dart
@@ -5,20 +5,21 @@ import 'package:presensi_flutter_test/config/api_config.dart';
 import 'package:presensi_flutter_test/models/login_request.dart';
 
 Future<String> login(LoginRequest loginRequest) async {
-  final url = Uri.parse('${ApiConfig.BASEAPIURL}/api/login');
+  final url = Uri.parse('${ApiConfig.BASEAPIURL}/api/login-student');
 
-  final response = await http.post(
-    url,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: jsonEncode(loginRequest.toJson())
-  );
+  final response = await http.post(url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode(loginRequest.toJson()));
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
     return data['data']['token'];
   } else {
+    print('Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
     throw Exception('Login gagal: ${response.body}');
   }
 }
