bb87b5315dcfa9187ef6fa241d1a5e66c580f96b
MrFatra <ramadhanfatra2004@gmail.com>
2025-05-17 11:10:27 +0700

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:presensi_flutter_test/views/home_page.dart';
-import 'package:presensi_flutter_test/views/login_page.dart';
-import 'package:presensi_flutter_test/views/permission.dart';
-import 'package:presensi_flutter_test/views/profile_siswa.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'package:presensi_flutter_test/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID');
   await dotenv.load(fileName: '.env');
-  WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('id', null);
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: RiwayatPeminjamanBukuPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
